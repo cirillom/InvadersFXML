668a8132f65be3e6f5f3997be95dbba1a419f748
cirillom <matheuscirillo@usp.br>
2022-12-30 23:52:35 -0300

    barrier javadoc

--- invaders/src/main/java/com/usp/elements/Barrier.java
+++ invaders/src/main/java/com/usp/elements/Barrier.java
@@ -7,14 +7,26 @@ import com.usp.App;
 
 import javafx.scene.image.Image;
 
+/**
+ * Barrier entity
+ */
 public class Barrier extends Sprite{
     String file[];
+    /**
+     * Barrier constructor
+     * @param x spawn x coordinates
+     * @param y spawn y coordinates
+     */
     public Barrier(int x, int y) {
         super(x, y, 15, 15, "barrier", "b_deg_1.png");
         file = new String[] {"b_dest.png", "b_deg_4.png","b_deg_3.png","b_deg_2.png"};
         this.life = file.length;        
     }
 
+    /**
+     * Changes the image of the barrier to show destruction
+     * @param file
+     */
     private void changeImage(String file){
         InputStream is = null;
         try {
@@ -26,6 +38,10 @@ public class Barrier extends Sprite{
         setImage(image);
     }
 
+    /**
+     * Damage override in order to change barrier image
+     */
+    @Override
     public void damage(){
         life--;
         if(life >= 0)
