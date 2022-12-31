9a1306f927e256630b1fc941d2f61784bc938b89
cirillom <matheuscirillo@usp.br>
2022-12-30 23:26:19 -0300

    javadoc

--- invaders/src/main/java/com/usp/engine/GameOverController.java
+++ invaders/src/main/java/com/usp/engine/GameOverController.java
@@ -7,8 +7,10 @@ import com.usp.App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+/**
+ * The menu controller only purpose is to initialize the actual game
+ */
 public class GameOverController {
-
     @FXML
     void startGame(ActionEvent event) throws IOException{
         App.setRoot("game");
