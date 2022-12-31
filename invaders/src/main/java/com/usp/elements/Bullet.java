9948a4ebf9d5519e32398ffe9798973436192403
cirillom <matheuscirillo@usp.br>
2022-12-30 23:51:29 -0300

    bullet javadoc

--- invaders/src/main/java/com/usp/elements/Bullet.java
+++ invaders/src/main/java/com/usp/elements/Bullet.java
@@ -1,40 +1,27 @@
 package com.usp.elements;
 
+/**
+ * Bullet entity
+ */
 public class Bullet extends Sprite {
 
+    /**
+     * Bullet constructor
+     * @param x spawn x coordinates
+     * @param y spawn y coordinates
+     * @param origin owner of this bullet (player or enemy)
+     */
     public Bullet(int x, int y, String origin) {
         super(x, y, 3, 18, origin + "bullet", "bullet.png");
         
+        //sets the direction the bullet should move
         if(origin.equals("player")){
             setDir(0, -1);
-        }
-        if(origin.equals("enemy")){
+        } else if(origin.equals("enemy")){
             setDir(0, 1);
         }
 
         this.speed = 5;
         this.life = 1;
-    }
-
-    public void move(){
-        if(xDir + yDir == 0) return;
-        
-        double xPos = getTranslateX();
-        double yPos = getTranslateY();
-        
-        if(xPos + xDir < 0 || xPos + xDir > (560 - getImage().getWidth())) {
-            life--;
-            return;
-        }
-        xPos += (xDir*speed);
-        setTranslateX(xPos);
-        
-        if(yPos + yDir < 0 || yPos + yDir > (640 - getImage().getHeight())) {
-            life--;
-            return;
-        }
-        
-        yPos += (yDir*speed);
-        setTranslateY(yPos);
     }
 }
